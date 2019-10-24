40953bcf91ba6b37655d7bc95735aa99a67b18f3
Julian <Iulian.Gherman@mycit.ie>
2019-10-24 15:02:34 +0100

    Added searching functionally

--- destinations/views.py
+++ destinations/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage , PageNotAnInteger
+from .choices import country_choices, price_choices
 
 from .models import Destination
 
 # Create your views here.
 
 def index(request):
-    destinations = Destination.objects.all()
+    destinations = Destination.objects.order_by('-list_date').filter(is_published=True)
 
-    paginator = Paginator(destinations, 1)
+    paginator = Paginator(destinations, 6)
     page = request.GET.get('page')
     paged_destinations = paginator.get_page(page)
 
@@ -18,7 +19,40 @@ def index(request):
     return render(request, 'destinations/destinations.html', context)
 
 def destination(request, destination_id):
-    return render(request, 'destinations/destination.html')
+    destination = get_object_or_404(Destination, pk=destination_id)
+
+    context = {
+        'destination': destination
+    }
+
+    return render(request, 'destinations/destination.html', context)
 
 def search(request):
-    return render(request, 'destinations/search.html')
+    queryset_list = Destination.objects.order_by('-list_date')
+
+    # Search by keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # Search by city
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # Search by price
+    if 'cost' in request.GET:
+        cost = request.GET['cost']
+        if cost:
+            queryset_list = queryset_list.filter(cost__lte=cost)
+
+    context = {
+        'country_choices': country_choices,
+        'price_choices': price_choices,
+        'destinations': queryset_list,
+        'values': request.GET
+    }
+
+    return render(request, 'destinations/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from destinations.choices import country_choices, price_choices
 
-# Create your views here.
+from destinations.models import Destination
 
 def index(request):
-    return render(request, 'pages/index.html')
+    destinations = Destination.objects.order_by('-list_date').filter(is_published=True)[:3] # Show only 3 destinations
+
+    context = {
+        'destinations': destinations,
+        'country_choices': country_choices,
+        'price_choices': price_choices
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/about.html')
